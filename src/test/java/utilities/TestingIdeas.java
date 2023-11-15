9264bbe2133cbc1e62d5d6ccf26a59e960f56443
glenn.mamaril <glenn.mamaril@pointwest.com.ph>
2023-11-16 07:39:19 +0800

    Trying to add Logs (WIP)

--- src/test/java/utilities/TestingIdeas.java
+++ src/test/java/utilities/TestingIdeas.java
@@ -6,16 +6,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.FileReader;
 import java.io.Reader;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.apache.logging.log4j.core.tools.Generate.CustomLogger;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class TestingIdeas {
+	private static final Logger LOGGER = Logger.getLogger(CustomLogger.class.getName());
+	private static String fileSeparator = File.separator;
+	
 	public static void main(String[] args) {
-		String fileSeparator = File.separator;
+//		String fileSeparator = File.separator;
 		String scrFilePath = System.getProperty("user.dir")+fileSeparator+"extentReports"+fileSeparator+"screenshots"+fileSeparator;
 		String tdFilePath = System.getProperty("user.dir")+fileSeparator+"testData"+fileSeparator;
 		System.out.println(scrFilePath);
@@ -27,12 +35,12 @@ public class TestingIdeas {
 		String jsonTestDataLoc = tdFilePath+"derulo.json";
 		String csvTestDataLoc = tdFilePath+"SampleTestData.csv";
 		
-//		String fileExtension = getFileExtension(new File(jsonTestDataLoc));
-//		System.out.println(fileExtension);
-//		fileExtension = getFileExtension(new File(csvTestDataLoc));
-//		System.out.println(fileExtension);
-//		fileExtension = getFileExtension(new File(System.getProperty("user.dir") + excelTestDataLoc));
-//		System.out.println(fileExtension);
+		String fileExtension = getFileExtension(new File(jsonTestDataLoc));
+		System.out.println(fileExtension);
+		fileExtension = getFileExtension(new File(csvTestDataLoc));
+		System.out.println(fileExtension);
+		fileExtension = getFileExtension(new File(System.getProperty("user.dir") + excelTestDataLoc));
+		System.out.println(fileExtension);
 		//String appiumVersion = getAppiumVersion();
 		//System.out.println(appiumVersion);
 		
@@ -42,17 +50,21 @@ public class TestingIdeas {
 //		String username = creds.testData(id, "username");
 //    	System.out.println(username);
     	//JSON
-    	JSONReader jsonFile = new JSONReader(jsonTestDataLoc);
-    	TestCaseData tcData = jsonFile.getTestCaseData("TC004");
-    	String TCUsername = tcData.getUsername();
-    	String TCPassword = tcData.getPassword();
-    	String TCSex = tcData.getSex();
-		System.out.println(TCUsername);
-		System.out.println(TCPassword);
-		System.out.println(TCSex);
-    	//CSV
-    	CSVReader csvCreds = new CSVReader(csvTestDataLoc);
-    	System.out.println(csvCreds.getData("TC002","password"));
+//    	JSONReader jsonFile = new JSONReader(jsonTestDataLoc);
+//    	TestCaseData tcData = jsonFile.getTestCaseData("TC004");
+//    	String TCUsername = tcData.getUsername();
+//    	String TCPassword = tcData.getPassword();
+//    	String TCSex = tcData.getSex();
+//		System.out.println(TCUsername);
+//		System.out.println(TCPassword);
+//		System.out.println(TCSex);
+//    	//CSV
+//    	CSVReader csvCreds = new CSVReader(csvTestDataLoc);
+//    	System.out.println(csvCreds.getData("TC002","password"));
+		configureLogger();
+		
+		LOGGER.info("Test Info");
+		LOGGER.warning("Test warning!");
 	}
 	
 	private static String getFileExtension(File file) {
@@ -87,5 +99,20 @@ public class TestingIdeas {
 			return null;
 		}
 		
+	}
+	
+	private static void configureLogger() {
+		try {
+			FileHandler fileHandler = new FileHandler(System.getProperty("user.dir")+fileSeparator+"testLogs"+fileSeparator+"test.log", true);
+			
+			SimpleFormatter formatter = new SimpleFormatter();
+			fileHandler.setFormatter(formatter);
+			
+			LOGGER.setLevel(Level.ALL);
+			
+			LOGGER.addHandler(fileHandler);
+		}catch(Exception e) {
+			LOGGER.log(Level.SEVERE, "Error configuring logger", e);
+		}
 	}
 }
